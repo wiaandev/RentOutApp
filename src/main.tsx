c438eb4db672aa5d44f26f6035220d4a9dfda1c5
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2024-09-15 20:19:32 +0200

    inti

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { RelayEnvironmentProvider } from "react-relay";
+import { RelayEnvironment } from "./RelayEnvironment";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <RelayEnvironmentProvider environment={RelayEnvironment}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </RelayEnvironmentProvider>
+);
