5a93450459cdb4870d46a54a9adfdf1a36a29589
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2024-11-27 11:02:27 +0200

    chore: register form layout

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -6,13 +6,12 @@ import {Radio, RadioGroup, TextField} from "@stackworx/react-hook-form-mui";
 import {PasswordTextField} from "../Components/Form/PasswordTextField.tsx";
 
 interface FormValues {
+    firstName: string;
+    lastName: string;
     email: string;
     password: string;
     confirmPassword: string;
-    profileType: {
-        renter: boolean;
-        rentee: boolean;
-    }
+    profileType: string;
 }
 
 
@@ -23,7 +22,7 @@ export const Register = () => {
         const form = useForm<FieldValues>({defaultValues});
 
         const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
-            console.log(data.email);
+            console.log(data);
         }
     return (<Grid2 container size={12} justifyContent={'center'} flexDirection={'column'} alignItems={'center'}>
         <img src={Logo} alt={'logo'} width={100}/>
@@ -31,12 +30,21 @@ export const Register = () => {
 
 
         <Form {...form} onSubmit={onSubmit}>
-            <TextField control={form.control} name={'email'} label={'Email'}/>
+            <Grid2 size={12} container direction={'column'}>
+
+                <Grid2 container size={'grow'}>
+                    <TextField control={form.control} name={'firstName'} label={'First Name'}/>
+                    <TextField control={form.control} name={'lastName'} label={'Last Name'}/>
+                    <TextField control={form.control} name={'email'} label={'Email'}/>
+
+                </Grid2>
             <PasswordTextField control={form.control} name={'password'} label={'Password'}/>
+            <PasswordTextField control={form.control} name={'confirmPassword'} label={'Confirm Password'}/>
             <RadioGroup name={'profileType'}>
                 <FormControlLabel label={'I am renting'} control={<Radio control={form.control}/>}/>
                 <FormControlLabel label={'I am buying'} control={<Radio control={form.control}/>}/>
             </RadioGroup>
+            </Grid2>
             <Button type={'submit'} variant={'contained'}>Submit</Button>
         </Form>
 
