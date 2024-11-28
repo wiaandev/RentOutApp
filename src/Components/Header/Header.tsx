3c9218003f81686d36f02a9eb4f1063dad8ab7a3
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2024-11-28 14:34:56 +0200

    chore: updated opacity

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,11 @@
-import {Grid2, Typography, Link as RouterLink} from "@mui/material";
+import {Grid2, Link as RouterLink} from "@mui/material";
 import Logo from '../../assets/logo.svg'
 import {Link} from "react-router-dom";
 
 export const Header = () =>  {
     return (
         <Grid2 size={12} container justifyContent={'space-between'} alignItems={'center'} position={'sticky'} top={0} zIndex={999} p={3} sx={{
-            background: 'rgba(255, 255, 255, 0.15)',
+            background: 'rgba(255, 255, 255, 0.45)',
             backdropFilter: 'blur(6.5px)',
             WebkitBackdropFilter: 'blur(6.5px)',
             border: '1px solid rgba(255, 255, 255, 0.18)',
